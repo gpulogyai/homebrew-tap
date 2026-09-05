f9f0419737b4fe6cd02f6a20bb2886e2468193ca
zayednetad <nur.ahmed@outlook.com>
2026-09-04 23:42:02 -0500

    ltb 1.1.80

--- Casks/ltb.rb
+++ Casks/ltb.rb
@@ -20,15 +20,15 @@
 # tap's cask_renames.json maps letthembuild -> ltb, which Homebrew resolves as
 # a rename and tells the user about.
 cask "ltb" do
-  version "1.1.79"
+  version "1.1.80"
 
   on_arm do
-    sha256 "95d9eb40e29dbf2eb5b320d004fa0f01a9cf1e5e362e7be827f936630b5e9798"
+    sha256 "16913c915f9917488cdff599480249e465d725b6fa0001518315a1cc158baf59"
 
     url "https://letthembuild.com/updates/LetThemBuild-#{version}-arm64-mac.zip"
   end
   on_intel do
-    sha256 "611df2e12c9e459470e10bdbd8af5b20825f8abbdd21c060f1c163a66aacc52a"
+    sha256 "9775297813867b6febe5b4002138dbd2d3146e76aed8f0088cb7496cc3445f4a"
 
     url "https://letthembuild.com/updates/LetThemBuild-#{version}-x64-mac.zip"
   end
